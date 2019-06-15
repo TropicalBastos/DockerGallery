8f9ca5f3b9f59ff09df2b814bbd2e4f0e89f82e5
Ian Bastos <ian-bastos@live.com>
2019-06-15 01:34:35 -0300

    initial services healthy

---
+++ imgservice/src/main/java/com/tropicalbastos/imggallery/ImgGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.tropicalbastos.imggallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgGalleryApplication.class, args);
+	}
+
+}
---
+++ imgservice/src/test/java/com/tropicalbastos/imggallery/ImgGalleryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tropicalbastos.imggallery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImgGalleryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
