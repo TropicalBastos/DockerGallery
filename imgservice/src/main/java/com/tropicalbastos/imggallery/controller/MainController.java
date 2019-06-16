5152cd830092da81a5340f914e9a3083be0c76a6
Ian Bastos <ian-bastos@live.com>
2019-06-16 01:54:49 -0300

    basic backend and migrations checked in

--- imgservice/src/main/java/com/tropicalbastos/imggallery/controller/MainController.java
+++ imgservice/src/main/java/com/tropicalbastos/imggallery/controller/MainController.java
@@ -3,7 +3,6 @@ package com.tropicalbastos.imggallery.controller;
 import java.util.List;
 import com.tropicalbastos.imggallery.model.Image;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
--- imgservice/src/main/resources/db/migration/V1__init.sql
+++ imgservice/src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,9 @@
-CREATE TABLE image (
-    id INT AUTOINCREMENT PRIMARY KEY,
-    path VARCHAR(255)
+CREATE TABLE `image` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    `path` VARCHAR(255)
 );
 
-INSERT INTO image (path) VALUES
+INSERT INTO `image` (`path`) VALUES
     ("https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"),
     ("https://cdn.pixabay.com/photo/2016/12/17/14/33/wave-1913559_960_720.jpg"),
     ("https://img.newatlas.com/juno-jupiter-15.jpg?auto=format%2Ccompress&ch=Width%2CDPR&fit=crop&h=347&q=60&rect=0%2C0%2C1918%2C1078&w=616&s=4492cd21f7e6d8e7b826d4e23a2198d2"),
